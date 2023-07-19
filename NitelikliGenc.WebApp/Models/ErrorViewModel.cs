c2a45eac36e1bcb3d19c5d7c326934443616ec7f
Levent KOMAN <levent.koman@kodpit.com>
2023-07-19 08:56:15 +0300

    Created MVC project. Html and Css examples was done.

---
+++ NitelikliGenc.WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace NitelikliGenc.WebApp.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
